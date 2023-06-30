8b8d040299313a39d066cbc7ba058962f77c9b58
LEEKH109 <dlrjsgml109@gmail.com>
2023-06-30 10:52:57 +0900

    while4

--- while4.py
+++ while4.py
@@ -1,8 +1,13 @@
-print('연습 문제: 제곱표(while)\n')
-input = int(input('input: '))
+number = 358
 
-num = 1
-while num <= input:
-    print(num)
-    print(num*num)
-    num += 1
+rem = rev = 0
+while number >= 1:
+    rem = number % 10
+    rev = rev * 10 + rem
+    number = number // 10
+
+print(rev)
+# START) num = 853
+# 1) rem = 8 rev = 8 num = 35
+# 2) rem = 5 rev = 85 num = 3
+# 3) rem = 3 rev = 853 num = 0 => STOP
